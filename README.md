adbd280982e022ff87bb546bf56b9996a41be39c
otavio-coding <otavio.a.f.97@gmail.com>
2023-12-06 15:40:49 -0300

    Small changes that don't change functionality
    Refactored start_menu and removed the print logo function.

--- README.md
+++ README.md
@@ -17,28 +17,25 @@ This Python script automates sending bulk messages on WhatsApp using Selenium. I
 2. **Download ChromeDriver:**
     Download the appropriate version of [ChromeDriver](https://sites.google.com/chromium.org/driver/) compatible with your Google Chrome browser version. Ensure that the Chromedriver executable is in your system's PATH.
 
-3. **Set Up Chrome User Profile:**
-    Update `CHROME_PROFILE_PATH` in the script with the path to your Chrome user profile.
-
-4. **Define CSV File:**
+3. **Define CSV File:**
     Replace `'ContactList.csv'` with the path to your CSV file containing contact numbers.
 
-5. **Define Message:**
+4. **Define Message:**
     Edit the `message.txt` file with the message you want to send.
 
-6. **Run the Script:**
+5. **Run the Script:**
     Execute the script:
     ```bash
     python WhatsappWebAutomation.py
     ```
 
-7. **QR Code Authentication:**
+6. **QR Code Authentication:**
     The script opens the WhatsApp web page, and if QR scanning is required, it waits for the user to scan the QR code.
 
-8. **Sending Messages:**
+7. **Sending Messages:**
     The script reads the CSV file, extracts phone numbers, and sends the predefined message to each contact. Any failed attempts are recorded.
 
-9. **Finish:**
+8. **Finish:**
     After sending messages, the script prints the phones that failed to receive the message.
 
 ## Important Notes
--- WhatsappWebAutomation.py
+++ WhatsappWebAutomation.py
@@ -12,25 +12,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-def print_logo():
-    print("""
- ██████╗██╗  ██╗██████╗  ██████╗ ███╗   ███╗███████╗
-██╔════╝██║  ██║██╔══██╗██╔═══██╗████╗ ████║██╔════╝
-██║     ███████║██████╔╝██║   ██║██╔████╔██║█████╗
-██║     ██╔══██║██╔══██╗██║   ██║██║╚██╔╝██║██╔══╝
-╚██████╗██║  ██║██║  ██║╚██████╔╝██║ ╚═╝ ██║███████╗
- ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝
- █████╗ ██╗   ██╗████████╗ ██████╗ ███████╗ █████╗ ██████╗
-██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗╚══███╔╝██╔══██╗██╔══██╗
-███████║██║   ██║   ██║   ██║   ██║  ███╔╝ ███████║██████╔╝
-██╔══██║██║   ██║   ██║   ██║   ██║ ███╔╝  ██╔══██║██╔═══╝
-██║  ██║╚██████╔╝   ██║   ╚██████╔╝███████╗██║  ██║██║
-╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝
-
-Not intended for spamming use consented data only.
-""")
-
-
 def qrcode_auth(driver):
     try:
         driver.get("https://web.whatsapp.com/")
@@ -71,24 +52,41 @@ def delete_chrome_profile(profile_path):
 
 
 def start_menu():
-    print_logo()
-    user = input('PRESS ENTER TO START\n\n')
+    print("""
+     ██████╗██╗  ██╗██████╗  ██████╗ ███╗   ███╗███████╗
+    ██╔════╝██║  ██║██╔══██╗██╔═══██╗████╗ ████║██╔════╝
+    ██║     ███████║██████╔╝██║   ██║██╔████╔██║█████╗
+    ██║     ██╔══██║██╔══██╗██║   ██║██║╚██╔╝██║██╔══╝
+    ╚██████╗██║  ██║██║  ██║╚██████╔╝██║ ╚═╝ ██║███████╗
+     ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝
+     █████╗ ██╗   ██╗████████╗ ██████╗ ███████╗ █████╗ ██████╗
+    ██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗╚══███╔╝██╔══██╗██╔══██╗
+    ███████║██║   ██║   ██║   ██║   ██║  ███╔╝ ███████║██████╔╝
+    ██╔══██║██║   ██║   ██║   ██║   ██║ ███╔╝  ██╔══██║██╔═══╝
+    ██║  ██║╚██████╔╝   ██║   ╚██████╔╝███████╗██║  ██║██║
+    ╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝
+
+    Not intended for spamming use consented data only.
+    """)
+
+    user = input('PRESS ENTER TO START')
 
     option = [""]
 
     def scan_new_qr():
         option[0] = "new_qr"
         root.destroy()
+
     def use_old_qr():
         option[0] = "same_qr"
         root.destroy()
 
     root = Tk()
-    frm = ttk.Frame(root, padding=50)
+    root.title('Start Menu')
+    frm = ttk.Frame(root, padding=30)
     frm.grid()
-
-    ttk.Button(frm, text="Scan a New QR Code", command=scan_new_qr).grid(column=0, row=0, pady=10)
-    ttk.Button(frm, text="Continue Without Scanning", command=use_old_qr).grid(column=0, row=1, pady=10)
+    ttk.Button(frm, text="New QR Code", command=scan_new_qr).grid(column=0, row=0, pady=10)
+    ttk.Button(frm, text="Continue", command=use_old_qr).grid(column=0, row=1, pady=10)
 
     root.mainloop()
 
@@ -96,54 +94,59 @@ def start_menu():
 
 
 def main():
-    # If new qr code is requested. Clean up the profile directory
-    if start_menu() == "new_qr":
+    try:
+        # If new qr code is requested. Clean up the profile directory
         profile_path = os.path.expanduser('~') + '\\AppData\\Local\\Google\\Chrome\\User Data\\Wtsp'
-        print("Cleaning up old QR settings")
-        delete_chrome_profile(profile_path)
+        if start_menu() == "new_qr":
+            print("Cleaning up old QR settings")
+            delete_chrome_profile(profile_path)
+        if start_menu() == "":
+            return
+        CHROME_PROFILE_PATH = 'user-data-dir=' + profile_path
 
-    CHROME_PROFILE_PATH = 'user-data-dir=' + os.path.expanduser('~') + '\\AppData\\Local\\Google' \
-                                                                       '\\Chrome\\User Data\\Wtsp'
-    options = webdriver.ChromeOptions()
-    options.add_argument(CHROME_PROFILE_PATH)
-    driver = webdriver.Chrome(options=options)
+        options = webdriver.ChromeOptions()
+        options.add_argument(CHROME_PROFILE_PATH)
+        driver = webdriver.Chrome(options=options)
 
-    config = load_config()
-    csv_file = config["csv_file"]
-    phone_row = config["phone_row"]
-    message_file = "message.txt"
-    
-    # Gets message from the message file
-    message = open(message_file, 'r', encoding='utf-8').read()
+        config = load_config()
+        csv_file = config["csv_file"]
+        phone_row = config["phone_row"]
+        message_file = "message.txt"
 
-    # Create empty list to store the phones that couldn't be reached
-    phones_that_failed = []
+        # Gets message from the message file
+        message = open(message_file, 'r', encoding='utf-8').read()
 
-    if qrcode_auth(driver) != 0:
+        # Create empty list to store the phones that couldn't be reached
+        phones_that_failed = []
 
-        print("QR code failed")
-    # Opens the web whatsapp page and waits for the user to scan the QR code
-    # if you have already done it recently just wait for one or two seconds
-    else:
-        # If QR scanning succeeds read the csv file with the contacts and star sending the messages
-        with open(csv_file, 'r', encoding='utf-8') as file:
-            reader = csv.reader(file, delimiter=';')
-            next(reader)  # skip header row
-            for row in reader:
-                phone = re.sub(r'\D', '', row[phone_row])
-                try:
-                    send_message(driver, phone, message)
-                except Exception as e:
-                    phones_that_failed.append(phone)
-                    print("Failed to send message to: " + phone)
+        if qrcode_auth(driver) != 0:
 
-    print("Finished sending the messages\n"
-          "Phones that failed: ")
-    print(phones_that_failed)
-    input("\nYou can close this window")
+            print("QR code failed")
+        # Opens the web whatsapp page and waits for the user to scan the QR code
+        # if you have already done it recently just wait for one or two seconds
+        else:
+            # If QR scanning succeeds read the csv file with the contacts and star sending the messages
+            with open(csv_file, 'r', encoding='utf-8') as file:
+                reader = csv.reader(file, delimiter=';')
+                next(reader)  # skip header row
+                for row in reader:
+                    phone = re.sub(r'\D', '', row[phone_row])
+                    try:
+                        send_message(driver, phone, message)
+                    except Exception as e:
+                        phones_that_failed.append(phone)
+                        print("Failed to send message to: " + phone)
 
-    while True:
-        pass
+        print("Finished sending the messages\n"
+              "Phones that failed: ")
+        print(phones_that_failed)
+        input("\nYou can close this window")
+
+        while True:
+            pass
+
+    except Exception as e:
+        print(f"Program Error:{e}\nPlease restart the app.")
 
 
 if __name__ == '__main__':
