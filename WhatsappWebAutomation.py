eff2fdeb47afe7fd7be367cbc8fe8edcd0e645b3
otavio-coding <otavio.a.f.97@gmail.com>
2023-12-06 12:28:31 -0300

    Add 'Scan new QR' function and improve send_message():
    start_menu() prompts the user to press ENTER. Then, a tkinter box appears with two buttons "Scan new qr" and "Continue without scanning"
    
    delete_chrome_profile() is called to delete the cached data stored in google chrome in order to allow the user to scan a new qrcode.
    
    send_message() now waits for the element 'main' and not 'side', because side appeared without the chat being ready causing failure to send message.

--- WhatsappWebAutomation.py
+++ WhatsappWebAutomation.py
@@ -1,11 +1,15 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import csv
+import json
+import os
+import re
 import time
 import urllib.parse
-import re
-import csv
-import os
-import json
+import shutil
+
+from tkinter import Tk, ttk
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 def print_logo():
@@ -23,6 +27,7 @@ def print_logo():
 ██║  ██║╚██████╔╝   ██║   ╚██████╔╝███████╗██║  ██║██║
 ╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝
 
+Not intended for spamming use consented data only.
 """)
 
 
@@ -40,16 +45,12 @@ def send_message(driver, phone, message):
     message = urllib.parse.quote(message)
     link = f"https://web.whatsapp.com/send?phone={phone}&text={message}"
     driver.get(link)
-    while len(driver.find_elements(By.ID, 'side')) < 1:
+
+    while len(driver.find_elements(By.ID, 'main')) < 1:
         time.sleep(1)
 
-    while len(driver.find_elements(By.LINK_TEXT, 'Iniciando conversa')) > 0:
-        time.sleep(1)
-
-    time.sleep(3)
-    send_message_button = driver.find_element(By.XPATH,
-                                              '//*[@id="main"]/footer/div[1]/div/span[2]'
-                                              '/div/div[2]/div[2]/button/span')
+    send_message_button = driver.find_element(By.XPATH,'//*[@id="main"]/footer/div[1]/div/span[2]'
+                                                       '/div/div[2]/div[2]/button/span')
     send_message_button.click()
     print('Message sent to:' + phone)
     time.sleep(2)
@@ -61,17 +62,53 @@ def load_config(file_path='config.json'):
     return config
 
 
-def main():
+def delete_chrome_profile(profile_path):
+    try:
+        shutil.rmtree(profile_path, ignore_errors=True)
+        print("Old QR settings cleaned")
+    except:
+        print("Couldn't generate ner QR, contact developer")
+
+
+def start_menu():
     print_logo()
-    user = input('PRESS ENTER TO START SENDING THE MESSAGES')
-    config = load_config()
+    user = input('PRESS ENTER TO START\n\n')
+
+    option = [""]
+
+    def scan_new_qr():
+        option[0] = "new_qr"
+        root.destroy()
+    def use_old_qr():
+        option[0] = "same_qr"
+        root.destroy()
+
+    root = Tk()
+    frm = ttk.Frame(root, padding=50)
+    frm.grid()
+
+    ttk.Button(frm, text="Scan a New QR Code", command=scan_new_qr).grid(column=0, row=0, pady=10)
+    ttk.Button(frm, text="Continue Without Scanning", command=use_old_qr).grid(column=0, row=1, pady=10)
+
+    root.mainloop()
+
+    return option[0]
+
+
+def main():
+    # If new qr code is requested. Clean up the profile directory
+    if start_menu() == "new_qr":
+        profile_path = os.path.expanduser('~') + '\\AppData\\Local\\Google\\Chrome\\User Data\\Wtsp'
+        print("Cleaning up old QR settings")
+        delete_chrome_profile(profile_path)
+
     CHROME_PROFILE_PATH = 'user-data-dir=' + os.path.expanduser('~') + '\\AppData\\Local\\Google' \
-                                                                       '\\Chrome\\User Data\\Wtsp' + user
+                                                                       '\\Chrome\\User Data\\Wtsp'
     options = webdriver.ChromeOptions()
     options.add_argument(CHROME_PROFILE_PATH)
     driver = webdriver.Chrome(options=options)
 
-    # Define the CSV file containing the phone numbers and the phone row
+    config = load_config()
     csv_file = config["csv_file"]
     phone_row = config["phone_row"]
     message_file = "message.txt"
@@ -103,7 +140,7 @@ def main():
     print("Finished sending the messages\n"
           "Phones that failed: ")
     print(phones_that_failed)
-    input("You can close this window")
+    input("\nYou can close this window")
 
     while True:
         pass
